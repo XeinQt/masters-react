871f3c157cdd213851857a18548567e8837984b5
My Name <myemail@example.com>
2025-09-04 02:05:13 +0800

    making a header main and footer in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Greeting from "./components/Greeting";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <div>
-      <Greeting />
+      <Header />
+      <MainContent />
+      <Footer />
     </div>
   );
 };
